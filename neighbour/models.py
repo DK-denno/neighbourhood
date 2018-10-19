52dae92e9f3358e85385c29c3811d41dcc0de2cc
DK-denno <dennisveer27@gmail.com>
2018-10-19 14:57:19 +0300

    working on models

--- neighbour/models.py
+++ neighbour/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Hotlines(models.Model):
+    name = models.CharField(max_length=20)
+    Hotline = models.BigIntegerField()
+
+
 class Neighbourhoods(models.Model):
     name = models.CharField(max_length=10)
     location =  models.CharField(max_length=10)
-    details =  models.CharField(max_length=40)
+    hotlines = models.ForeignKey(Hotlines,related_name='neigbourhoods')
+    
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
@@ -21,3 +28,9 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.user.username
+
+class Businesses(models.Model):
+    name = models.CharField(max_length=10)
+    details = models.CharField(max_length=100)
+    neighbourhood = models.ForeignKey(Neighbourhoods,related_name='businesses')
+    

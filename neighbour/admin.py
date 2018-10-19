c9d147b36815214c1a6f3917ba3c143c2756564d
DK-denno <dennisveer27@gmail.com>
2018-10-19 15:00:14 +0300

    displayed the hotlines model

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile,Neighbourhoods
+from .models import Profile,Neighbourhoods,Hotlines,Businesses
 # Register your models here.
 
 admin.site.register(Profile)

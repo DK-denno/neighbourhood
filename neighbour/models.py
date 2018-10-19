00804131032a47d5fbc312dbf5cd90e89533b76d
DK-denno <dennisveer27@gmail.com>
2018-10-19 12:08:26 +0300

    made first migration to the database

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Neighbourhoods(models.Model):
+    name = models.CharField(max_length=10)
+    location =  models.CharField(max_length=10)
+    details =  models.CharField(max_length=40)
+    
